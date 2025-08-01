4b384c334c8c4cde51cbb61cea154113e368d461
MAkhilKumar <murupojuakhilkumar@gmail.com>
2025-08-01 13:33:14 +0530

    Update eligibleToVote.java

--- Basic programs/eligibleToVote.java
+++ Basic programs/eligibleToVote.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class eligibleToVote {
     public static void main(String[] args) {
       Scanner input = new Scanner (System.in);
       int n=input.nextInt(); 
