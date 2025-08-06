70b5ffde1c7c9e89cefc898579c1b833b778e168
MAkhilKumar <murupojuakhilkumar@gmail.com>
2025-08-06 16:29:08 +0530

    Update and rename Main (4).java to SubtracttionOfTwoMatrices.java

--- Basic programs/SubtracttionOfTwoMatrices.java
+++ Basic programs/SubtracttionOfTwoMatrices.java
@@ -1,15 +1,10 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
 
-*******************************************************************************/
 import java.util.*;
-public class Main
+public class SubtractionOfTwoMatrices
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 	    Scanner scan=new Scanner(System.in);
 		System.out.print("Enter Rows : ");
 		int rows=scan.nextInt();
