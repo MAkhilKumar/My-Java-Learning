00c72112f13dcac5816d34d4b99f38ac3017adc0
MAkhilKumar <murupojuakhilkumar@gmail.com>
2025-08-06 14:02:03 +0530

    Update and rename Main (3).java to AdditionOfTwoMatrices.java

--- Basic programs/AdditionOfTwoMatrices.java
+++ Basic programs/AdditionOfTwoMatrices.java
@@ -1,15 +1,8 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 import java.util.*;
-public class Main
+public class AdditionOfTwoMatrices
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 	    Scanner scan=new Scanner (System.in);
 	    
 	    int[][] mat1=new int[2][2];
