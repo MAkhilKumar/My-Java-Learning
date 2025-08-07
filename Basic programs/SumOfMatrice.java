e936574fb8246e2a0a602ec2479e11786df3fbad
MAkhilKumar <murupojuakhilkumar@gmail.com>
2025-08-07 12:45:17 +0530

    Update and rename Main (5).java to SumOfMatrice.java

---
+++ Basic programs/SumOfMatrice.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class SumOfMatrice
+{
+	public static void main(String[] args)
+	{
+	    Scanner scan=new Scanner(System.in);
+	    int sum=0;
+	    int [][]array=new int [3][3];
+	    int rows=array.length;
+	    int col=array[0].length;
+	    int[][] result=new int [3][3];
+		System.out.print("Enter Matrice Input  : ");
+		for(int i=0;i<rows;i++)
+		{
+		    for(int j=0;j<col;j++)
+		    {
+		        array[i][j]=scan.nextInt();
+		        sum =sum+ array[i][j];
+		    }
+		    
+		}
+		System.out.print("The Sum Of Matrice is : "sum);
+	}
+}
