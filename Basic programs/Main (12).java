2d71443b38421132b199b0e07f931954088138dd
MAkhilKumar <murupojuakhilkumar@gmail.com>
2025-08-08 14:31:51 +0530

    Add files via upload

---
+++ Basic programs/Main (12).java
@@ -0,0 +1,47 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scan=new Scanner(System.in);
+	   
+		System.out.print("Enter Rows Number : ");
+		int rows=scan.nextInt();
+		System.out.print("Enter Colums Number : ");
+		int col=scan.nextInt();
+		int [][] array=new int [rows][col];
+		int [][] result=new int [rows][col];
+     	int row1=0;
+     	int row2=2;
+     	System.out.println("Enter Array Input ");
+     	for(int i=0;i<rows;i++)
+     	{
+     	    for(int j=0;j<col;j++)
+     	    {
+     	    array[i][j]=scan.nextInt();
+     	    }
+     	}
+		for(int i=0;i<rows;i++)
+		{
+		   result[i][i]= array[row1][i];
+		   array[row1][i]= array[row2][i];
+		   array[row2][i]= result[i][i];
+		   
+		}
+		for(int i=0;i<rows;i++)
+		{
+		    for(int j=0;j<col;j++)
+		    {
+		        System.out.print(array[i][j]+" ");
+		    }
+		    System.out.println();
+		}
+	}
+}
