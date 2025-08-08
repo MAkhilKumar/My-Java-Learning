fa216d2dd1dff4cf0347b953d551fab2461d4aa2
MAkhilKumar <murupojuakhilkumar@gmail.com>
2025-08-08 13:51:17 +0530

    Add files via upload

---
+++ Basic programs/Main (10).java
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scan=new Scanner(System.in);
+	    int secondiagonal=0;
+	    int back=2;
+		System.out.print("Enter Row NUmber ");
+		int rows=scan.nextInt();
+		System.out.print("Enter Colums Number ");
+		int col=scan.nextInt();
+		int [][]array = new int [rows][col];
+		for(int i=0;i<rows;i++)
+		{
+		    for(int j=0;j<col;j++)
+		    {
+		        array[i][j]=scan.nextInt();
+		    }
+		}
+		for(int i=0;i<rows;i++)
+		{
+		  
+		      secondiagonal= secondiagonal+ array[i][back];
+		      back--;
+		    
+		}
+		System.out.println("Second Diagonal sum is " +secondiagonal);
+	}
+}
