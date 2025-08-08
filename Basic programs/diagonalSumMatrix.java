81f78b602732bd0f666001de0935b3d8df880bd0
MAkhilKumar <murupojuakhilkumar@gmail.com>
2025-08-08 13:52:40 +0530

    Update and rename Main (10).java to diagonalSumMatrix.java

--- Basic programs/diagonalSumMatrix.java
+++ Basic programs/diagonalSumMatrix.java
@@ -1,15 +1,8 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 import java.util.*;
 public class Main
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 	    Scanner scan=new Scanner(System.in);
 	    int secondiagonal=0;
 	    int back=2;
