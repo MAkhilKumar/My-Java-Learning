7267f342db869eab6a323e7057967f33ffadb272
MAkhilKumar <murupojuakhilkumar@gmail.com>
2025-08-11 13:41:17 +0530

    Add files via upload

---
+++ Basic programs/Main (17).java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scan=new Scanner(System.in);
+	    int [][] matrix=new int [3][3];
+	    int row=matrix.length;
+	    int col=matrix[0].length;
+	    System.out.println("Enter Array Input ");
+		for(int i=0;i<row;i++)
+		{
+    	    for(int j=0;j<col;j++)
+    		{
+    		       matrix[i][j]=scan.nextInt(); 
+    		}
+		}
+		for(int j=0;j<col;j++)
+		{
+    		for(int i=0;i<row;i++)
+    		{
+    	        System.out.print(matrix[i][j]+" ");
+    		}
+		System.out.println();
+		}
+	}
+}
