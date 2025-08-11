1c4c960f992cfdae9dc4766ea577423c3761721b
MAkhilKumar <murupojuakhilkumar@gmail.com>
2025-08-11 13:16:08 +0530

    Add files via upload

---
+++ Basic programs/Main (16).java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Main
+{
+		public static void main(String[] args) {
+	    Scanner scan=new Scanner(System.in);
+	    System.out.print("Enter Number : ");
+	    int no=scan.nextInt();
+		 for(int i=0;i<no;i++)
+          {
+              for(int j=i;j<no;j++)
+              {
+                  System.out.print(" ");
+              }
+              for(int j=0;j<2*i-1;j++)
+              {
+                  System.out.print("*");
+              }
+              System.out.println();
+          }
+          for(int i=0;i<no;i++)
+          {
+              for(int j=0;j<i;j++)
+              {
+                  System.out.print(" ");
+              }
+              for(int j=0;j<(2*(no-i)-1);j++)
+              {
+                  System.out.print("*");
+              }
+              System.out.println();
+          }
+        
+	}
+}
+
+
