ec6f7dd8b271c1453cebf86068a51d9cb1b35fcf
Akhil Kumar <murupojuakhilkumar@gmail.com>
2025-08-14 12:53:57 +0530

    Add files via upload

---
+++ Basic programs/Main (19).java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner input = new Scanner (System.in);
+	    System.out.println(" enter first side of square");
+		int a = input.nextInt();
+		int average = ( a * a) ;
+		System.out.println(" Area Of Square is "+average);
+	}
+}
