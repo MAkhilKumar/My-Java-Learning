6df513e6b910e95f6dbf9a5880cc66c031d47fd4
Akhil Kumar <murupojuakhilkumar@gmail.com>
2025-08-14 12:55:50 +0530

    Update and rename Main (19).java to AreaOfSquare.java

---
+++ Basic programs/AreaOfSquare.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class AreaOfSquare
+{
+	public static void main(String[] args) 
+	{
+	    Scanner input = new Scanner (System.in);
+	    System.out.println(" enter first side of square");
+		int a = input.nextInt();
+		int area = ( a * a) ;
+		System.out.println(" Area Of Square is "+area);
+	}
+}
