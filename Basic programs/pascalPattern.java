841e119f350b220efc46b4d343702b574a6217b2
Akhil Kumar <murupojuakhilkumar@gmail.com>
2025-08-20 20:24:12 +0530

    Add files via upload

---
+++ Basic programs/pascalPattern.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class pascalPattern
+{
+	public static void main(String[] args)
+	{
+	    Scanner input=new Scanner (System.in);
+		System.out.print("enter number : ");
+		int no=input.nextInt();
+		for(int i=1;i<=no;i++)
+		{
+		    for(int j=1;j<=no-i;j++)
+		    {
+		        System.out.print(" ");
+		    }
+		    
+		    for(int j=1;j<=i;j++)
+		    {
+		        System.out.print(i+" ");
+		    }
+		    System.out.println();
+		}
+	}
+}
