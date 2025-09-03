74c3b59fc5f316830f04a212478c1eb09a63a31a
Akhil Kumar <murupojuakhilkumar@gmail.com>
2025-09-03 20:09:36 +0530

    Add files via upload

---
+++ Basic programs/NumberPatternDecreasing.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class NumberPatternDecreasing
+{
+	public static void main(String[] args)
+	{
+	        Scanner scan=new Scanner(System.in);
+		    System.out.print("Enter Number : ");
+		    int num=scan.nextInt();
+	     	for(int i=1;i<=num;i++)
+		{
+		    for(int j=1;j<=num-i+1;j++)
+		  {
+		    System.out.print(j+" ");
+		  }
+		    System.out.println();
+		}
+	}
+}
