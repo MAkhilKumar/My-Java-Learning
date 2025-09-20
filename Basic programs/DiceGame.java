80a0d3a55801061318f31540a0b4febc988bc9ae
Akhil Kumar <murupojuakhilkumar@gmail.com>
2025-09-20 21:57:21 +0530

    Add files via upload

---
+++ Basic programs/DiceGame.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class DiceGame
+{
+	public static void main(String[] args)
+	{
+	    Scanner input=new Scanner (System.in);
+		System.out.print("Guess the dice  number : ");
+		int number=input.nextInt();
+		Random ran=new Random();
+		int dice=ran.nextInt(6);
+		
+		if(number == dice)
+		{
+		    System.out.println("Congratulations its correct !"+dice);
+		} else
+		{
+		    System.out.println("Better luck next time ");
+		}
+		System.out.println(dice);
+	}
+}
