2f6623b163cfff1c5f3b61f1e4c9506ec4886576
Akhil Kumar <murupojuakhilkumar@gmail.com>
2025-09-29 22:38:28 +0530

    Update and rename DecimalToBinary (1).java to DecimalToBinary.java

--- Basic programs/DecimalToBinary.java
+++ Basic programs/DecimalToBinary.java
@@ -2,18 +2,20 @@ import java.util.*;
 public class DecimalToBinary
 {
     
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		Scanner scan=new Scanner(System.in);
 		int a=scan.nextInt();
         int power=0;
         int deci=0;
         int myNum=deci;
-        while(a>0) {
+        while(a>0)
+			{
             int rem=a%2;
             deci=deci+(rem) * (int) Math.pow(10,power);
             power++;
             a=a/2;
-        }
+            }
         System.out.println("Decimal To Binary is "+deci);
     
 	}
